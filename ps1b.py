10f1d5f99cbc64dcb7b55ca880d58db92383e608
ROADKILLSANTA <ericnie7@gmail.com>
2017-07-01 21:50:22 -0400

    Create ps1b.py

---
+++ ps1b.py
@@ -0,0 +1,18 @@
+total_cost=float(input("Cost of your dream house: "))
+portion_down_payment=.25
+current_savings=0
+r=.04
+annual_salary=float(input("Annual Salary: "))
+portion_saved=float(input("Portion saved every year"))
+months=0
+down=portion_down_payment*total_cost
+semi_annual_raise=float(input("Semi-Annual raise: "))
+
+while(current_savings<down):
+  current_savings+=current_savings*r/12
+  current_savings+=annual_salary/12*portion_saved
+  months+=1
+  if(months%6==0):
+    savings+=semi_annual_raise
+print("Number of months: "+str(months))
+print(str(int(months/12//1))+" Years | "+str(months%12)+" Months")
