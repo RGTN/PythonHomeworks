b233b0f180b3151eb5948eaecd4b596908d845e1
ROADKILLSANTA <ericnie7@gmail.com>
2017-07-02 14:04:52 -0400

    Update ps1c.py

--- ps1c.py
+++ ps1c.py
@@ -3,7 +3,7 @@ portion_down_payment=.25
 current_savings=0
 r=.04
 annual_salary=float(input("Annual Salary: "))
-portion_saved=float(input("Portion saved every year"))
+portion_saved=random.randint(0,10000)
 months=0
 down=portion_down_payment*total_cost
 semi_annual_raise=.07
@@ -13,7 +13,4 @@ while(current_savings<down):
 	current_savings+=annual_salary/12*portion_saved
 	months+=1
 	if(months%6==0):
-	savings+=semi_annual_raise
-
-print("Number of months: "+str(months))
-print(str(int(months/12//1))+" Years | "+str(months%12)+" Months")
+		savings+=semi_annual_raise
